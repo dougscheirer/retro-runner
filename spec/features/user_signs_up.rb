3f1aa4b05976a4fcc1dd65e4b4a0402c69f59f19
Doug Scheirer <dscheirer@perforce.com>
2015-06-15 21:53:57 -0700

    light mods to refactor the helper method

--- spec/features/user_signs_up.rb
+++ spec/features/user_signs_up.rb
@@ -26,12 +26,4 @@ feature 'User signs up' do
     expect(page).to have_content('Log in')
   end
 
-  def sign_up_with(email, name, password)
-    visit signup_path
-    fill_in 'Email', with: email
-    fill_in 'Name', with: name
-    fill_in 'Password', with: password
-    fill_in 'Confirm password', with: password
-    click_button 'Submit'
-  end
 end
