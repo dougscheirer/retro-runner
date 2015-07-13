ecf99575ffb3aadee609fba35139f5f0d6b47707
Alexander Shankland <ashankland@dhcp-27-n100.dhcp.perforce.com>
2015-07-13 16:34:16 -0700

    more tests, fixed javascript issue

--- app/assets/javascripts/issues.js
+++ app/assets/javascripts/issues.js
@@ -2,22 +2,22 @@
 
 $(function() {
     $(".tbody").append("content");
-    function startTimer(duration, display) {
-        var timer = duration, minutes, seconds;
-        setInterval(function () {
-            minutes = parseInt(timer / 60, 10);
-            seconds = parseInt(timer % 60, 10);
-
-            minutes = minutes < 10 ? "0" + minutes : minutes;
-            seconds = seconds < 10 ? "0" + seconds : seconds;
-
-            display.textContent = minutes + ":" + seconds + " seconds remaining, ";
-
-            if (--timer < 0) {
-                display.textContent = "";
-            }
-        }, 1000);
-    }
-    startTimer(30, document.querySelector('#time'));
 });
 
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    var interval_id = setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds + " seconds remaining, ";
+
+        if (--timer < 0) {
+            display.textContent = "";
+        }
+    }, 1000);
+};
--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -3,12 +3,21 @@ require 'rails_helper'
 RSpec.describe VotesController, :type => :controller do
 
   let(:valid_session) { { 'user_id' => 1 } }
+  let(:valid_session_2) { { 'user_id' => 2} }
 
   let(:valid_attributes) {
     {
         :issue_id => 1,
         :retro_id => 1,
         :user_id => 1
+    }
+  }
+
+  let(:valid_attributes_2) {
+    {
+        :issue_id => 1,
+        :retro_id => 1,
+        :user_id => 2
     }
   }
 
@@ -26,11 +35,12 @@ RSpec.describe VotesController, :type => :controller do
     @retro.save
     @user = FactoryGirl.create(:user)
     @user.save
+    @user2 = FactoryGirl.create(:user)
+    @user2.save
     @issue = FactoryGirl.create(:issue)
     @issue.save
   end
 
-  let(:valid_session) { { :user_id => 1 } }
 
   describe "GET new" do
     it "returns http success" do
@@ -76,8 +86,28 @@ RSpec.describe VotesController, :type => :controller do
 
       it "redirects to the vote's issue" do
         post :create, {:vote => valid_attributes, :issue_id => 1}, valid_session
-        expect(response).to redirect_to(Retro.find(1))
+        expect(flash[:success]).to be_present
       end
+    end
+  end
+
+  describe "GET clear_all" do
+    it "deletes all votes belonging to the right user" do
+      post :create, {:vote => valid_attributes, :issue_id => 1}, valid_session
+      post :create, {:vote => valid_attributes, :issue_id => 1}, valid_session
+      post :create, {:vote => valid_attributes, :issue_id => 1}, valid_session
+      expect(Vote.count).to be(3)
+      get :clear_all, valid_attributes
+      expect(Vote.count).to be(0)
+    end
+    it "deletes only votes belonging to the right user" do
+      expect(Vote.count).to be(0)
+      post :create, {:vote => valid_attributes, :issue_id => 1}, valid_session
+      post :create, {:vote => valid_attributes_2, :issue_id => 1}, valid_session_2
+      expect(Vote.where(user_id: 2).count).to be(1)
+      expect(Vote.count).to be(2)
+      get :clear_all, valid_attributes
+      expect(Vote.count).to be(1)
     end
   end
 
