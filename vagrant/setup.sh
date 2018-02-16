9057702c134e463cf23845d4e1245cfdbb44ee8f
dscheirer <dscheirer@perforce.com>
2018-02-16 03:03:30 -0800

    initial vagrant commit

---
+++ vagrant/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+yum install -y git-core zlib zlib-devel gcc-c++ patch readline readline-devel libyaml-devel libffi-devel openssl-devel make bzip2 autoconf automake libtool bison postgresql-devel wget
+
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+
+curl -sSL https://get.rvm.io | bash -s stable --ruby
+
+wget http://nodejs.org/dist/latest/node-v9.5.0-linux-x64.tar.gz
+tar --strip-components 1 -xzvf node-v* -C /usr/local
+
